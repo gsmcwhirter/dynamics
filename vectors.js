12a9614cc2cfe022daf988da622ac18c7a338c25
Gregory McWhirter <greg@hallofkvasir.org>
2013-03-05 14:13:41 -0800

    Adding differential vector field

--- vectors.js
+++ vectors.js
@@ -2,10 +2,32 @@ var Generator = require("./generator")
   , inherit = require("inherit")
   ;
   
-function ContinuousReplicatorVector(game, start){
+function ContinuousReplicatorVector(game, start, options){
+  this.game = extend({
+    "tl-r": 0
+  , "tl-c": 0
+  , "tr-r": 0
+  , "tr-c": 0
+  , "bl-r": 0
+  , "bl-c": 0
+  , "br-r": 0
+  , "br-c": 0
+  }, game);
   
+  this.options = extend({
+    debug: false
+  }, options);
+  
+  this.start = extend({x:0, y:0}, start); //start should be an object with .x and .y for coordinate properties
 }
 
 module.exports = ContinuousReplicatorVector;
 
 inherit(ContinuousReplicatorVector, Generator);
+
+ContinuousReplicatorVector.prototype.generate = function (){
+  var dxy = this.dxydt(this.start.x, this.start.y);
+  
+  this.emit("point", {x: this.start.x + dxy[0], y: this.start.y + dxy[1]});
+  this.emit("done");
+};
